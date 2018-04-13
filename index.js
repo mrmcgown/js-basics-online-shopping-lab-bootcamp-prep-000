1ba1f8ede653881e1d8cc1595daf15831f6b56f9
Michael R. McGown <mrmcgown@gmail.com>
2018-04-13 13:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  cartTotal = total();
+  
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
