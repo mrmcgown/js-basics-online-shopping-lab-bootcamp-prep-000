987aa3fe2ee417f9385fbd5b0cc5fe25601a261d
Michael R. McGown <mrmcgown@gmail.com>
2018-04-13 13:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
-    
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-  } cart === [];
+    cartFun = total();
+    return `Your total cost is $${cartFun()}, which will be charged to the card ${cardNumber}.`;
+  }
 }
