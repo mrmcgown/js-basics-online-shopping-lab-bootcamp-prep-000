bb0f36e8d2f67705b1cc9af377948d84c6b858b9
Michael R. McGown <mrmcgown@gmail.com>
2018-04-13 13:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ function placeOrder(cardNumber) {
   } else {
     
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-  } cart = []
+  } cart = [];
 }
