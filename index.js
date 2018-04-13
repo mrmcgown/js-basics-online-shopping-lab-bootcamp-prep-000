8cb7b3ffe3fce2003128f9209e6d92b244e74cb3
Michael R. McGown <mrmcgown@gmail.com>
2018-04-13 13:14:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,11 +47,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var index = cart.indexOf(item);
-  if (index !== -1) {
-    cart.splice(index, 1);
-  } else {
-    return `That item is not in your cart.`;
+  var itemFound = false;
+  for (var i=0; i < cart.length; i++) {
+    if (cart[i].itemName === item); {
+      cart.splice(i,1);
+      itemFound === true;
+    }
+  }
+    if (itemFound === false) {
+      return `That item is not in your cart.`;
   }
 }
 
